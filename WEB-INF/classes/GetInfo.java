e9ae7896ef6351c9ddafb1fa712e8f77694479fc
BHolmwood <bholmwoo@ualberta.ca>
2014-11-19 14:36:45 -0700

    Added my files. Put appropriate files in WEB_INF and update web.xml for your files.

---
+++ WEB-INF/classes/GetInfo.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+/**
+ *  This servlet sends one picture stored in the table below to the client 
+ *  who requested the servlet.
+ *
+ *   picture( photo_id: integer, title: varchar, place: varchar, 
+ *            sm_image: blob,   image: blob )
+ *
+ *  The request must come with a query string as follows:
+ *    GetOnePic?12:        sends the picture in sm_image with photo_id = 12
+ *    GetOnePicture?big12: sends the picture in image  with photo_id = 12
+ *
+ *  @author  Li-Yan Yuan
+ *
+ */
+public class GetInfo extends HttpServlet 
+    implements SingleThreadModel {
+
+    /**
+     *    This method first gets the query string indicating PHOTO_ID,
+     *    and then executes the query 
+     *          select image from yuan.photos where photo_id = PHOTO_ID   
+     *    Finally, it sends the picture to the client
+     */
+
+    public void doGet(HttpServletRequest request,
+		      HttpServletResponse response)
+	throws ServletException, IOException {
+	
+	//  construct the query  from the client's QueryString
+	String picid  = request.getQueryString();
+	String query;
+
+	query = "select owner_name, subject, place, timing, description from images where photo_id="
+	        + picid;
+
+	//ServletOutputStream out = response.getOutputStream();
+	PrintWriter out = response.getWriter();
+
+	/*
+	 *   to execute the given query
+	 */
+	Connection conn = null;
+	try {
+	    conn = getConnected();
+	    Statement stmt = conn.createStatement();
+	    ResultSet rset = stmt.executeQuery(query);
+	    response.setContentType("text/html");
+            String owner_name, subject, place, timing, description;
+
+	    if ( rset.next() ) {
+	    	owner_name = rset.getString("owner_name");
+	    	subject = rset.getString("subject");
+	        place = rset.getString("place");
+	        timing = rset.getString("timing");
+	        description = rset.getString("description");	        
+                out.println("<html><head><title>"+owner_name + "'s photo " + "</title>+</head>" +
+	                 "<body bgcolor=\"#000000\" text=\"#cccccc\">" +
+		 "<center><img src = \"/proj1/GetOnePic?"+picid+"\">" +
+			 "<h3>Subject: " + subject +" </h3>" +
+			 "<h3>Location: " + place + " </h3>" +
+			 "<h3>Owner: " + owner_name + " </h3>" +
+			 "<h3>Date: " + timing + " </h3>" +
+			 "<h3>Description: " + description + " </h3>" +
+			 "</body></html>");
+            }
+	    else
+	      out.println("<html> Pictures are not avialable</html>");
+	} catch( Exception ex ) {
+	    out.println(ex.getMessage() );
+	}
+	// to close the connection
+	finally {
+	    try {
+		conn.close();
+	    } catch ( SQLException ex) {
+		out.println( ex.getMessage() );
+	    }
+	}
+    }
+
+    /*
+     *   Connect to the specified database
+     */
+    private Connection getConnected() throws Exception {
+
+    	String username = "kboyle";
+    	String password = "kieran92";
+            /* one may replace the following for the specified database */
+    	String dbstring = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+    	String driverName = "oracle.jdbc.driver.OracleDriver";
+	/*
+	 *  to connect to the database
+	 */
+	Class drvClass = Class.forName(driverName); 
+	DriverManager.registerDriver((Driver) drvClass.newInstance());
+	return( DriverManager.getConnection(dbstring,username,password) );
+    }
+}
---
+++ WEB-INF/classes/PictureBrowse.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import oracle.jdbc.driver.*;
+import java.text.*;
+import java.net.*;
+
+/**
+ *  A module to display pictures.
+ *  Adapted from Li-Yan Yuan's example.
+ *
+ *  @author  Benjamin Holmwood
+ *
+ */
+public class PictureBrowse extends HttpServlet implements SingleThreadModel {
+    
+    /**
+     *  Generate and then send an HTML file that displays all the thermonail
+     *  images of the photos.
+     *
+     *  Both the thermonail and images will be generated using another 
+     *  servlet, called GetOnePic, with the photo_id as its query string
+     *
+     */
+    public void doGet(HttpServletRequest request,
+		      HttpServletResponse res)
+	throws ServletException, IOException {
+
+	//  send out the HTML file
+	res.setContentType("text/html");
+	PrintWriter out = res.getWriter ();
+
+	out.println("<html>");
+	out.println("<head>");
+	out.println("<title> Photo List </title>");
+	out.println("</head>");
+	out.println("<body bgcolor=\"#000000\" text=\"#cccccc\" >");
+	out.println("<center>");
+	out.println("<h3>The List of Images </h3>");
+
+	/*
+	 *   to execute the given query
+	 */
+	try {
+		//TODO: get query from session
+	    String query = "select photo_id from images";
+
+	    Connection conn = getConnected();
+	    Statement stmt = conn.createStatement();
+	    ResultSet rset = stmt.executeQuery(query);
+	    String p_id = "";
+
+	    while (rset.next() ) {
+		p_id = (rset.getObject(1)).toString();
+
+	       // specify the servlet for the image
+               out.println("<a href=\"/proj1/GetInfo?"+p_id+"\">");
+	       // specify the servlet for the themernail
+	       out.println("<img src=\"/proj1/GetOnePic?"+p_id +
+	                   "\"></a>");
+	    }
+	    stmt.close();
+	    conn.close();
+	} catch ( Exception ex ){ out.println( ex.toString() );}
+    //TODO change exception
+	out.println("<P><a href=\"/yuan/servlets/logicsql.html\"> Return </a>");
+	out.println("</body>");
+	out.println("</html>");
+    }
+    
+    /*
+     *   Connect to the specified database
+     */
+    private Connection getConnected() throws Exception {
+
+	String username = "kboyle";
+	String password = "kieran92";
+        /* one may replace the following for the specified database */
+	//String dbstring = "jdbc.logicsql@luscar.cs.ualberta.ca:2000:database";
+	//String driverName = "com.shifang.logicsql.jdbc.driver.LogicSqlDriver";
+	String dbstring = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+	String driverName = "oracle.jdbc.driver.OracleDriver";
+	/*
+	 *  to connect to the database
+	 */
+	Class drvClass = Class.forName(driverName); 
+	DriverManager.registerDriver((Driver) drvClass.newInstance());
+	return( DriverManager.getConnection(dbstring,username,password) );
+    }
+}
+
+
+
+
